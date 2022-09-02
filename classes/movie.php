521a129addc58210a11df6d539654bd1228ed0f5
LucaSpennato <luca.spennato@gmail.com>
2022-09-02 16:52:13 +0200

    class add contructor, functions

--- classes/movie.php
+++ classes/movie.php
@@ -1,14 +1,33 @@
 <?php
+
     class Movie {
         private $title;
         private $trama;
-        private $actors = [];
-        private $directors = [];
-        private $producers = [];
-        private $company;
-        private $releaseYear;
-
+        private $director;
+        // private $actors = [];
+        // private $producers = [];
+        // private $company;
+        // private $releaseYear;
+        // private $companyEmail;
         
+        function __construct($_title, $_trama, $_director)
+        {
+            $this->title = $_title;
+            $this->trama = $_trama;
+            $this->company = $_director;
+        }
+
+        public function getTitle(){
+            return $this->title;
+        }
+
+        public function getTrama(){
+            return $this->trama;
+        }
+
+        public function getDirector(){
+            return $this->director;
+        }
      
     }
 ?>
