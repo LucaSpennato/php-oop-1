45d91bdf355d7b7819073c29e43f17e0a360236e
LucaSpennato <luca.spennato@gmail.com>
2022-09-02 15:58:18 +0200

    setting variables in movie class

--- classes/movie.php
+++ classes/movie.php
@@ -1,3 +1,14 @@
 <?php
-    
+    class Movie {
+        private $title;
+        private $trama;
+        private $actors = [];
+        private $directors = [];
+        private $producers = [];
+        private $company;
+        private $releaseYear;
+
+        
+     
+    }
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    require_once __DIR__ . '/classes/movie.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
